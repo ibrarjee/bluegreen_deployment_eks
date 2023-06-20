f3974c2b6deed956302005d84ed2031ef90374a6
Ibrar <ibrar.ullah@eurustechnologies.com>
2023-06-21 03:59:25 +0500

    script for changing after every the run successful

--- after_green_updatedns.sh
+++ after_green_updatedns.sh
@@ -2,8 +2,8 @@
 
 # Define the Kubernetes service name and AWS Route 53 hosted zone ID
 hosted_zone_id="Z074130239BXW7W2J0TY9"
-record_name="bluegreen.groveops.net"
-namespace="dev"
+record_name="testingbluegreen.groveops.net"
+namespace="green"
 ingress_name="ingress-2048"
 
 # Get the external IP address of the Kubernetes service
