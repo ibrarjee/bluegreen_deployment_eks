a1368f66df6779e0a5376537f50b734c23b867ca
Ibrar <ibrar.ullah@eurustechnologies.com>
2023-06-21 22:46:41 +0500

    secondary routes added

--- namespace.sh
+++ namespace.sh
@@ -1,12 +1,24 @@
-#!/bin/bash
-namespace="blue"
+# #!/bin/bash
+# namespace="blue"
 
-# Check if the namespace already exists
+# # Check if the namespace already exists
     
-if kubectl get namespace "$namespace" &>/dev/null; then
-    echo "Namespace '$namespace' already exists. Skipping creation."
+# if kubectl get namespace "$namespace" &>/dev/null; then
+#     echo "Namespace '$namespace' already exists. Skipping creation."
+# else
+#     # Create the namespace
+#     kubectl create namespace "$namespace"
+#     echo "Namespace '$namespace' created successfully."
+# fi
+
+#!/bin/bash
+
+NAMESPACE="blue"
+
+# Check if namespace exists
+if kubectl get namespaces | grep -q $NAMESPACE; then
+  echo "Namespace $NAMESPACE already exists, skipping..."
 else
-    # Create the namespace
-    kubectl create namespace "$namespace"
-    echo "Namespace '$namespace' created successfully."
+  echo "Creating namespace $NAMESPACE..."
+  kubectl create namespace $NAMESPACE
 fi
